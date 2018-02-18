cec3e95815fa349da9ff33bff6d6dfa28f74f74f
mkylychev <mkylychev@gmail.com>
2018-02-18 20:54:44 +0600

    add authorization

--- app/src/main/java/com/vkappkg/vk/vk/MainActivity.java
+++ app/src/main/java/com/vkappkg/vk/vk/MainActivity.java
@@ -1,10 +1,16 @@
 package com.vkappkg.vk.vk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 import com.vk.sdk.util.VKUtil;
+import com.vkappkg.vk.vk.consts.ApiConstants;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -15,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VKSdk.login(this, ApiConstants.DEFAULT_LOGIN_SCOPE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // User passed Authorization
+            }
+            @Override
+            public void onError(VKError error) {
+                // User didn't pass Authorization
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
---
+++ app/src/main/java/com/vkappkg/vk/vk/consts/ApiConstants.java
@@ -0,0 +1,9 @@
+package com.vkappkg.vk.vk.consts;
+
+/**
+ * Created by myrzabek on 2/18/18.
+ */
+
+public class ApiConstants {
+    public static final String[] DEFAULT_LOGIN_SCOPE = {};
+}
