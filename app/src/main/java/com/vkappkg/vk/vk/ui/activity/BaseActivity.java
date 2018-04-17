240e087b5cf1916f06dc528575954b0bae823fde
mkylychev <mkylychev@gmail.com>
2018-04-17 23:05:45 +0600

    add custom fragment manager

--- app/src/main/java/com/vkappkg/vk/vk/ui/activity/BaseActivity.java
+++ app/src/main/java/com/vkappkg/vk/vk/ui/activity/BaseActivity.java
@@ -2,7 +2,12 @@ package com.vkappkg.vk.vk.ui.activity;
 
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.vkappkg.vk.vk.ui.fragment.BaseFragment;
 
 public class BaseActivity extends MvpAppCompatActivity {
+
+    public void fragmentOnScreen(BaseFragment fragment){
+
+    }
 
 }
