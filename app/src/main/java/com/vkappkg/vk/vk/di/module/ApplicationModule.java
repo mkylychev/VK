2fc793934a35a35048d36db7991c972be61baed9
mkylychev <mkylychev@gmail.com>
2018-12-12 18:04:57 +0600

    add typeFonts

--- app/src/main/java/com/vkappkg/vk/vk/di/module/ApplicationModule.java
+++ app/src/main/java/com/vkappkg/vk/vk/di/module/ApplicationModule.java
@@ -3,6 +3,7 @@ package com.vkappkg.vk.vk.di.module;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Typeface;
 
 import javax.inject.Singleton;
 
@@ -22,5 +23,11 @@ public class ApplicationModule {
     @Provides
     public Context provideContext(){
         return mApplication;
+    }
+
+    @Singleton
+    @Provides
+    Typeface provideTypeFace(Context context){
+        return Typeface.createFromAsset(context.getAssets(),"MaterialIcons-Regular.ttf");
     }
 }
